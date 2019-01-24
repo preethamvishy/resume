2c331a2c4bb5d5872886e48c9446bd9c863dd198
Preetham Viswanathan <preethamvishy10@gmail.com>
2019-01-24 03:08:51 -0500

    update

---
+++ src/app/data.ts
@@ -0,0 +1,22 @@
+export let sections = [
+    {
+      name: 'TECHNOLOGY',
+      icon: 'glyphicon-cog'
+    },
+    {
+      name: 'EDUCATION',
+      icon: 'glyphicon-education'
+    },
+    {
+      name: 'WORK',
+      icon: 'glyphicon-briefcase'
+    },
+    {
+      name: 'OPEN SOURCE',
+      icon: 'glyphicon-console'
+    },
+    {
+      name: 'AWARDS',
+      icon: 'glyphicon-user'
+    },
+  ]
--- src/app/sections/sections.component.ts
+++ src/app/sections/sections.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as data from '../data'
 @Component({
   selector: 'app-sections',
   templateUrl: './sections.component.html',
   styleUrls: ['./sections.component.scss']
 })
 export class SectionsComponent implements OnInit {
-
-  constructor() { }
+  sections;
+  constructor() { 
+    this.sections = data.sections;
+  }
 
   ngOnInit() {
   }
